67f3a5b73d13242f9903a53ae6df371e5843d837
Sergey Postument <sergey.postument@gmail.com>
2016-04-10 22:19:48 +0300

    v0.2a, moving to MongoDB

--- db.py
+++ db.py
@@ -22,16 +22,16 @@ def filling_gamer_ids(start_id, end_id, ids_per_request):
             ids_list = str(list(range(start_id, start_id+ids_per_request)))[1:-1]
         else:
             ids_list = str(list(range(start_id, end_id)))[1:-1]
-        stats = wot.get_player_personal_data(ids_list)  # JSON with Users from WG API
-        for item in stats['data'].items():  # item = one user from WG API
-            if item[1] is not None:  # item[1] = json data of one user
+        stats = wot.get_player_personal_data(ids_list)
+        for item in stats['data'].items():
+            if item[1] is not None:
                 if db.users.find({'account_id': item[1]['account_id']}).count() > 0:
                     print('FOUNDED, will REPLACE')
                 else:
                     print('Cant search USER in Database, will ADD')
                     res = db.users.insert_one(item[1])
         start_id += ids_per_request
-    return print("------------------------- Finished filling")
+    return print("Finished filling")
 
 filling_gamer_ids(200, 220, 10)
 
