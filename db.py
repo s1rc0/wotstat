b0ed967138c58cef493420ab9491ae3de9fe9117
Sergey Postument <sergey.postument@gmail.com>
2016-04-11 21:01:10 +0300

    v0.2a, moving to MongoDB

--- db.py
+++ db.py
@@ -31,12 +31,3 @@ def filling_gamer_ids(start_id, end_id, ids_per_request):
     return print("Finished filling")
 
 # filling_gamer_ids(1, 1000, 100)
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
